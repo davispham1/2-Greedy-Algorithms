2f7541e8318c133820757ffd7b9cd3500f62f32c
davispham1 <davispham1126@gmail.com>
2026-03-05 19:43:37 -0500

    main, with tests

--- src/main.py
+++ src/main.py
@@ -1,5 +1,15 @@
-def main():
+from FIFO import fifo
+from LRU import lru
+from OPTFF import optff
+import sys
 
+def main():
+    k, m = map(int, input().split())
+    requests = list(map(int, input().split()))
+
+    print(f"FIFO  : {fifo(k, requests)}")
+    print(f"LRU   : {lru(k, requests)}")
+    print(f"OPTFF : {optff(k, requests)}")
 
 if __name__ == "__main__":
     main()
